12c49c9ef533f5ea16512d7564b4eebd1e5b8c7d
Toyesh002 <79399571+Toyesh002@users.noreply.github.com>
2021-04-15 14:59:56 +0530

    Add files via upload

---
+++ fbnum.py
@@ -0,0 +1,13 @@
+# Uses python3
+def calc_fib(n):
+    if(n<=1):
+        return n
+    prevnum=0
+    curnum=1
+    for i in range(1,n):
+        prevprevnum=prevnum
+        prevnum=curnum
+        curnum=prevnum+prevprevnum
+    return curnum
+n = int(input())
+print(calc_fib(n))
